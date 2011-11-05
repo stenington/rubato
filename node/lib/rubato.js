ad7072b5c39212002f402a9fe5d112e13737b735
Mike Larsson <mikelarssonftw@gmail.com>
2011-11-05 17:28:05 -0400

    cleaning up some extraneous stuff for official v1

--- node/lib/rubato.js
+++
@@ -1,6 +0,0 @@
-exports.RequestHandler = function RequestHandler() {
-  return function handler(req, res) {
-    res.writeHead('404 Not Found', {'Content-Type': 'text/plain'});
-    res.end('NO U\n');
-  };
-};
